110aad13d0c4cd070305fc39d85f66b23180fc69
IgorSchittekat <igor.schittekat@gmail.com>
2017-11-09 16:10:05 +0100

    added bin, build, cmake, CMakeLists.txt

---
+++ build/src/CMakeFiles/Gradius.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Gradius.dir/main.cpp.o"
+  "Gradius.pdb"
+  "Gradius"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Gradius.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
