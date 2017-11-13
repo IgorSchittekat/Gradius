fb68a264b65477061337bd51c4fabcfc75c9b95a
IgorSchittekat <igor.schittekat@gmail.com>
2017-11-13 14:36:37 +0100

    Added Enemy, Bullet, Obstacle classes

---
+++ src/Enemy.h
@@ -0,0 +1,20 @@
+#ifndef GRADIUS_ENEMY_H
+#define GRADIUS_ENEMY_H
+
+
+#include "Entity.h"
+
+class Enemy : public Entity {
+public:
+
+
+
+private:
+
+
+
+
+};
+
+
+#endif //GRADIUS_ENEMY_H
---
+++ src/Obstacle.h
@@ -0,0 +1,19 @@
+#ifndef GRADIUS_OBSTACLE_H
+#define GRADIUS_OBSTACLE_H
+
+
+#include "Entity.h"
+
+class Obstacle : public Entity {
+public:
+
+
+
+private:
+
+
+
+};
+
+
+#endif //GRADIUS_OBSTACLE_H
