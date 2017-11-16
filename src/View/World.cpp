052ce969c517b765896517a9f17a2ba93d46c238
IgorSchittekat <igor.schittekat@gmail.com>
2017-11-16 16:35:00 +0100

    Added world

---
+++ src/View/World.cpp
@@ -0,0 +1,18 @@
+#include "World.h"
+
+
+World::World() {
+
+}
+
+World::World(const World &rhs) {
+
+}
+
+World &World::operator=(const World &rhs) {
+    return *this;
+}
+
+World::~World() {
+
+}
---
+++ src/View/World.h
@@ -0,0 +1,20 @@
+#ifndef GRADIUS_WORLD_H
+#define GRADIUS_WORLD_H
+
+
+class World {
+public:
+    World();
+
+    World(const World &rhs);
+
+    World &operator=(const World &rhs);
+
+    ~World();
+
+private:
+
+};
+
+
+#endif //GRADIUS_WORLD_H
